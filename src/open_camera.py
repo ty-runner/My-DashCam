040704fe837b9b6a4291409ce751fdd4d987523f
ty-runner <ty.runner02@gmail.com>
2024-07-14 09:22:22 -0700

    Added display components

--- src/open_camera.py
+++ src/open_camera.py
@@ -4,8 +4,15 @@ from datetime import datetime
 from screeninfo import get_monitors
 def gen_filename():
 	return datetime.now().strftime("%Y-%m-%d_%H-%M-%S.h264")
-	
-	
+
+def get_speed():
+    #will be the gps speed module
+    return 50
+def get_overlay_text():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    speed = get_speed()
+    return f"{current_time} \n {speed} mph"
+
 with picamera.PiCamera() as camera:
 	monitor = get_monitors()[0]
 	screen_width = monitor.width
@@ -14,7 +21,6 @@ with picamera.PiCamera() as camera:
 	camera.rotation=90
 	camera.resolution = (screen_width, screen_height)
 	camera.start_preview(fullscreen=True)
-	#camera.start_preview(fullscreen=False, window=(0,0,400,1200))
 	time.sleep(2)
 	
 	try:
@@ -22,7 +28,11 @@ with picamera.PiCamera() as camera:
 			filename = gen_filename()
 			camera.start_recording(filename)
 			print("Recording... :")
-			camera.wait_recording(300)
+			start_time = time.time()
+			while time.time() - start_time < 300:
+				overlay_text = get_overlay_text()
+				camera.annotate_text = overlay_text
+				time.sleep(1)  
 			camera.stop_recording()
 			print("Stopped")
 			
@@ -30,3 +40,5 @@ with picamera.PiCamera() as camera:
 		camera.stop_recording()
 		camera.stop_preview()
 		print("Keyboard interrupt")
+
+
